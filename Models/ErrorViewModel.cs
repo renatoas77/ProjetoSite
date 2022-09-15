e2ebe4ca11b68bcfa836ec975d8615bec13bee37
renatoas77 <renato.antonio7@hotmail.com>
2022-09-15 18:27:31 -0300

    Iniciando o versionamento do meu projeto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FighteR_PG.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
