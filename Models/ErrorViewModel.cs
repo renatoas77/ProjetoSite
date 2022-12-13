d844a8c7906a495fe3504bcf190959e1f746414c
renatoas77 <renato.antonio7@hotmail.com>
2022-12-12 23:02:22 -0300

    Terminada a view HUD

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -2,7 +2,7 @@ namespace FighteR_PG.Models
 {
     public class ErrorViewModel
     {
-        public string? RequestId { get; set; }
+        public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
